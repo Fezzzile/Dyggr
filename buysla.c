2a319f6436c52e4fe1a655450c797672e8bb091e
Fezile Nkuna <fezile_nkuna@hotmail.com>
2024-04-23 00:13:35 +0200

    Much better. Now reading from file and writing to a file (instead of stdin and out; CLI arguments); Restores WAVEs are bit-perfect)

--- buysla.c
+++ buysla.c
@@ -3,97 +3,86 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* I think trying to read every tag in the header was a mistake.
- * I think I'm gonna read the first 16 bytes (the variants mainly differ in bytes 5-8
- * If they match a WAV file
- * Read further until I reach the "data" chunk
- * then read the next four bytes to see how many bytes to write to file
- */
-// Microsoft Wave
-struct waveheader_t {
-	// All values, save the name of the chunks, are in little-endian mode
-	char *waveChunk; // Beginning of the "fmt " subchunk
-	uint32_t waveSectionSize; // 0x01000000 (16) for PCM
-	uint16_t waveType; // 0x0100 for linear PCM; other values may be other forms of PCM, such as APCM
-	uint16_t channelTotal; // 2 for stereo
-	uint32_t samplesPerSecond; // Example: 44100, 48000, 96000
-	uint32_t bytesPerSecond; // Usually 176400
-	uint16_t blockAlignment; // = channelTotal * bytesPerSample (or bitsPerSampe / 8)
-	uint16_t bitsPerSample; // Bit depth
-	char *dataChunk; // Beginning of the "data" subchunk
-	uint32_t rawDataSizeInBytes; // Size of the whole file minus the header
-};
-typedef struct waveheader_t waveheader;
 
-int main(){
+int main(int argc, char *argv[]){
 	int carve = 0;
+	if (argc < 2){
+		fprintf(stdout, "Buysla: Restore deleted files (currently WAVE only)\n\n");
+		fprintf(stdout, "Copyright (c) 2024 Fezile Nkuna\n\n");
+		fprintf(stdout, "Usage: %s <path to device or file>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
 	while (carve != EOF){
 		char riff[12];
 		char tempriff[12];
 		char cmpriff[] = "RIFF";
 		char cmpwave[] = "WAVE";
+		char cmpdata[] = "data";
+		char data[4];
+		uint32_t rawSizeInBytes;
+		int numBytesBeforeDataChunk = 0;
+		FILE *restoredfile = fopen("Restored.wav", "a"); // Currently all files are appended to this file; need to fix
+		FILE *DeviceOrFile = fopen(argv[1], "r");
 		
 		while(1){
 			int queryfound = 0;
 
 			// Start searching here
-			for (int i = 0; i < 12 ; i++){
-				carve = getc(stdin);
+			        carve = getc(DeviceOrFile);
 				if (carve == EOF){
 					fprintf(stdout, "Reached end of file.\n");
 					exit(0);
 				}
 				riff[11] = carve;
 				if (memcmp(riff, cmpriff, 4) == 0 && memcmp(&riff[8], cmpwave, 4) == 0){
-					fprintf(stdout, "Found the query at riff[%d-%d]\n", i, i + 11);
-					queryfound = 1;
-					break;
+				        fprintf(stdout, "Found RIFF header.\n");
+					// Write header first
+					for (int v = 0; v < 12; v++){
+						fprintf(restoredfile, "%c", riff[v]);
+					}
+				//	break;
+					while(1){
+						carve = getc(DeviceOrFile);
+						if (carve == EOF){
+							fprintf(stdout, "Reached end of file.\n");
+							exit(0);
+						}
+
+						putc(carve, restoredfile); // Write the bytes between the "fmt " and "data" chunks
+						data[3] = carve;
+						if (memcmp(data, cmpdata, 4) == 0){
+							//puts("Data chunk found");
+							fread(&rawSizeInBytes, 1, 4, DeviceOrFile);
+							fprintf(stdout, "Found %d MiB WAVE file.\n", (rawSizeInBytes/1024)/1024);
+							
+							// Write raw data size, following "data" chunk. 
+							// Bitshifting to the right because of little-endianness.
+							// I am sure there's a libC function that does this quicker.
+							fputc(rawSizeInBytes & 0x000000ff, restoredfile);
+							fputc((rawSizeInBytes & 0x0000ff00) >> 8, restoredfile);
+							fputc((rawSizeInBytes & 0x00ff0000) >> 16, restoredfile);
+							fputc((rawSizeInBytes & 0xff000000) >> 24, restoredfile);
+
+							for(int h = 0; h < rawSizeInBytes; h++){
+								putc(getc(DeviceOrFile), restoredfile); // Append the raw audio data
+							}
+							break; // Break loop to avoid trailing bytes 
+						}
+						// Search: shift array contents leftward
+						// to avoid overriding, since I write only to the last position
+						for (int m = 0; m < 3; m++){
+							data[m] = data[m + 1];
+						}
+
+					}
 				}
-				// Shift array contents
+				// Shift array contents leftward
 				for (int b = 0; b < 11; b++){
 					riff[b] = riff[b + 1];
 				}
-			}
-			if (queryfound){
-				break;
-			}
 		}
-
-		// Outside searching loop
-		for (int k = 0; k < 12; k++){
-			fprintf(stdout, "%c", riff[k]);
-		}
-		waveheader waveaudio;
-                // Fill waveChunk 
-		for (int l = 0; l < 4; l++){
-			getc(stdin);	
-		}
-		waveaudio.waveChunk = "fmt ";
-		//fprintf(stdout, "waveaudio.waveChunk = ");
-	       	fprintf(stdout, "%s", waveaudio.waveChunk);
-		
-		puts("");
-
-		// Final version will read from a file, not standard-in.
-		fread(&waveaudio.waveSectionSize, 1, 4, stdin);
-		fread(&waveaudio.waveType, 1, 2, stdin);
-		fread(&waveaudio.channelTotal, 1, 2, stdin); 
-		fread(&waveaudio.samplesPerSecond, 1, 4, stdin);
-		fread(&waveaudio.bytesPerSecond, 1, 4, stdin);
-		fread(&waveaudio.blockAlignment, 1, 2, stdin);
-		fread(&waveaudio.bitsPerSample, 1, 2, stdin);
-		waveaudio.dataChunk = "data";
-		getc(stdin);
-		getc(stdin);
-		getc(stdin);
-		getc(stdin);
-		fread(&waveaudio.rawDataSizeInBytes, 1, 4, stdin); 
-
-		puts("");
-		fprintf(stdout, "waveaudio.rawDataSizeInBytes = %d\n", waveaudio.rawDataSizeInBytes);
 
 	}
-		return 0;
+		return EXIT_SUCCESS;
 }
-	
-
