2541cd5aafd876428a80a5b16a2ace9270553383
Fezile Nkuna <fezile_nkuna@hotmail.com>
2024-04-23 15:31:53 +0200

    Formatting; need to configure Neovim

--- buysla.c
+++ buysla.c
@@ -26,7 +26,6 @@ int main(int argc, char *argv[]){
 	
 		for (int i = 1; i < argc; i++){
 			if (strcmp(argv[i], "-s") == 0 /*|| strcmp(argv[i], "--source=<source>") == 0*/){
-
 				if ((i + 1) == argc){
 					fprintf(stderr, "Enter source from which to carve files.\n");
 					// Usage(..)
@@ -38,7 +37,7 @@ int main(int argc, char *argv[]){
 		}
 
 		FILE *restoredfile = fopen("Restored.wav", "a"); // Currently all files are appended to this file; need to fix
-		FILE *DeviceOrFile = fopen(source /*rgv[1]*/, "r");
+		FILE *DeviceOrFile = fopen(source, "r");
 		if (DeviceOrFile == NULL){
 			fprintf(stderr, "Error opening %s.\n", source);
 			// Usage(..)
@@ -46,17 +45,14 @@ int main(int argc, char *argv[]){
 		}
 		
 		while(1){
-			int queryfound = 0;
-
-			// Start searching here
-			        carve = getc(DeviceOrFile);
+				carve = getc(DeviceOrFile);
 				if (carve == EOF){
 					fprintf(stdout, "End of %s reached.\n", source);
 					exit(0);
 				}
 				riff[11] = carve;
 				if (memcmp(riff, cmpriff, 4) == 0 && memcmp(&riff[8], cmpwave, 4) == 0){ /* WAVE header found. */
-					// Write header first
+					/* Write "RIFF....WAVE" first  */
 					for (int v = 0; v < 12; v++){
 						fprintf(restoredfile, "%c", riff[v]);
 					}
@@ -66,17 +62,17 @@ int main(int argc, char *argv[]){
 							fprintf(stdout, "End of %s reached.\n", source);
 							exit(0);
 						}
-
-						putc(carve, restoredfile); // Write the bytes between the "fmt " and "data" chunks
+						/* Keep writing bytes until the "data" chunk is reached */
+						putc(carve, restoredfile); 
 						data[3] = carve;
-						if (memcmp(data, cmpdata, 4) == 0){
-							//puts("Data chunk found");
+						if (memcmp(data, cmpdata, 4) == 0){ /* "data" chunk found */
 							fread(&rawSizeInBytes, 1, 4, DeviceOrFile);
 							fprintf(stdout, "%d MiB WAVE file found.\n", (rawSizeInBytes/1024)/1024);
 							
-							// Write raw data size, following "data" chunk. 
-							// Bitshifting to the right because of little-endianness.
-							// I am sure there's a libC function that does this quicker.
+							/* Write raw data size
+							 * By shifting to the right because of little-endianness
+							 * Surely there is a libC function that does this more efficiently.
+							 */
 							fputc(rawSizeInBytes & 0x000000ff, restoredfile);
 							fputc((rawSizeInBytes & 0x0000ff00) >> 8, restoredfile);
 							fputc((rawSizeInBytes & 0x00ff0000) >> 16, restoredfile);
@@ -85,17 +81,17 @@ int main(int argc, char *argv[]){
 							for(int h = 0; h < rawSizeInBytes; h++){
 								putc(getc(DeviceOrFile), restoredfile); // Append the raw audio data
 							}
-							break; // Break loop to avoid trailing bytes 
+							break; /* Break loop to avoid trailing bytes */
 						}
-						// Search: shift array contents leftward
-						// to avoid overriding, since I write only to the last position
+						/* Search: shift array contents leftward
+						 * to avoid overriding, since bytes are written on the the last position.
+						 */
 						for (int m = 0; m < 3; m++){
 							data[m] = data[m + 1];
 						}
-
 					}
 				}
-				// Shift array contents leftward
+				/* Shift array contents leftward */
 				for (int b = 0; b < 11; b++){
 					riff[b] = riff[b + 1];
 				}
