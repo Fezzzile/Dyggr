251e17725ddeb5bdc90a015d1246cbea8f237ea8
Fezile Nkuna <fezile_nkuna@hotmail.com>
2024-04-23 13:17:57 +0200

    No more segfault. --source removed. Some touchups.

--- buysla.c
+++ buysla.c
@@ -3,13 +3,12 @@
 #include <string.h>
 #include <stdlib.h>
 
-// Just testing branching
 int main(int argc, char *argv[]){
 	int carve = 0;
 	if (argc < 2){
 		fprintf(stdout, "Buysla: Restore deleted files (currently WAVE only)\n\n");
 		fprintf(stdout, "Copyright (c) 2024 Fezile Nkuna\n\n");
-		fprintf(stdout, "Usage: %s <path to device or file>\n", argv[0]);
+		fprintf(stdout, "Usage: %s -s <source>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
@@ -26,20 +25,23 @@ int main(int argc, char *argv[]){
 		char *source;
 	
 		for (int i = 1; i < argc; i++){
-			if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--source") == 0){
+			if (strcmp(argv[i], "-s") == 0 /*|| strcmp(argv[i], "--source=<source>") == 0*/){
+
 				if ((i + 1) == argc){
 					fprintf(stderr, "Enter a source (call Usage(..))\n");
 					exit(EXIT_FAILURE);
 				}
 				source = argv[i + 1];
-				fprintf(stdout, "The source you entered is %s\n", source);
-				fprintf(stdout, "argc = %d\n", argc);
 				break;
 			}
 		}
 
 		FILE *restoredfile = fopen("Restored.wav", "a"); // Currently all files are appended to this file; need to fix
 		FILE *DeviceOrFile = fopen(source /*rgv[1]*/, "r");
+		if (DeviceOrFile == NULL){
+			fprintf(stderr, "Error opening file. Call Usage(..)\n");
+			exit(EXIT_FAILURE);
+		}
 		
 		while(1){
 			int queryfound = 0;
@@ -47,21 +49,19 @@ int main(int argc, char *argv[]){
 			// Start searching here
 			        carve = getc(DeviceOrFile);
 				if (carve == EOF){
-					fprintf(stdout, "Reached end of file.\n");
+					fprintf(stdout, "End of %s reached.\n", source);
 					exit(0);
 				}
 				riff[11] = carve;
-				if (memcmp(riff, cmpriff, 4) == 0 && memcmp(&riff[8], cmpwave, 4) == 0){
-				        fprintf(stdout, "Found RIFF header.\n");
+				if (memcmp(riff, cmpriff, 4) == 0 && memcmp(&riff[8], cmpwave, 4) == 0){ /* WAVE header found. */
 					// Write header first
 					for (int v = 0; v < 12; v++){
 						fprintf(restoredfile, "%c", riff[v]);
 					}
-				//	break;
 					while(1){
 						carve = getc(DeviceOrFile);
 						if (carve == EOF){
-							fprintf(stdout, "Reached end of file.\n");
+							fprintf(stdout, "End of %s reached.\n", source);
 							exit(0);
 						}
 
@@ -70,7 +70,7 @@ int main(int argc, char *argv[]){
 						if (memcmp(data, cmpdata, 4) == 0){
 							//puts("Data chunk found");
 							fread(&rawSizeInBytes, 1, 4, DeviceOrFile);
-							fprintf(stdout, "Found %d MiB WAVE file.\n", (rawSizeInBytes/1024)/1024);
+							fprintf(stdout, "%d MiB WAVE file found.\n", (rawSizeInBytes/1024)/1024);
 							
 							// Write raw data size, following "data" chunk. 
 							// Bitshifting to the right because of little-endianness.
